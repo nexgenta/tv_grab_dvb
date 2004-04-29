afb23f82cf4085391a3da36e8a912790bd3b9de6
mhb <mhb@7385db4a-20e2-4f61-964a-c837765cb39d>
2004-04-29 23:00:59 +0000

    Initial revision
    git-svn-id: http://www.darkskiez.co.uk/svn/tv_grab_dvb/trunk@11 7385db4a-20e2-4f61-964a-c837765cb39d

---
+++ lookup.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lookup.h"
+
+extern char* lookup(struct lookup_table *l, int id) {
+	
+//	printf("Looked up %x", id);
+	for(;(l->id != id)&&(l->id != -1);l++) {
+
+	}
+	if (l->desc==NULL) {
+		static char number[255];
+		sprintf(number,"Unknown ID: %x",id);
+		return (char*)strdup(number);
+	}
+	return (l->desc);
+}
